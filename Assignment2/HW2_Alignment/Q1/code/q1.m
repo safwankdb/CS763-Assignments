30d0c28c76f7475f6f09c346a72ca3aeb9eb32ef
kushagra1729 <kuku12320@gmail.com>
2019-02-04 00:06:44 +0530

    Assignment2 Q1(a) done and verified

---
+++ Assignment2/HW2_Alignment/Q1/code/q1.m
@@ -0,0 +1,13 @@
+% Check normalise2D
+% pts2=[0 1
+%     1 0
+%     1 1];
+% [normal,T]=normalise2D(pts2) 
+
+% Check normalise3D
+% pts2=[0 1
+%     1 0
+%     0 0 
+%     1 1];
+% [normal,T]=normalise3D(pts2) 
+
