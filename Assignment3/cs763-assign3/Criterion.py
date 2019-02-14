a1b8e9cc101c83b2f2a4b30d2fead5fd2a5f4c12
skq024 <36313756+skq024@users.noreply.github.com>
2019-02-14 15:49:58 +0530

    Update criterion.py

--- Assignment3/cs763-assign3/Criterion.py
+++ Assignment3/cs763-assign3/Criterion.py
@@ -4,8 +4,17 @@ from torch.autograd import variable
 
 class Criterion:
     def forward(inp, target):
-        out = input * torch.diag(target)
-        return -torch.mean(torch.log(out))
+        new = torch.zeros_like(target)
+        for i in range(target.size()[0]):
+            new[i][0] = inp[i][target[i]]
+        return -torch.mean(torch.log(new))
 
     def backward(input, target):
-        
+        new = torch.zeros_like(target)
+        for i in range(target.size()[0]):
+            new[i] = -1 / (inp[i][target[i]])
+        grad = new / target.size()[0]
+        new1 = torch.zeros_like(input)
+        for i in range(target.size()[0]):
+            new1[i][target[i]] = grad[i]
+        return new1
