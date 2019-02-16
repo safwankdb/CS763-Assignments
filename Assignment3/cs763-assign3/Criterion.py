706a2da5b843445a6d29388bb1893ad54254705c
skq024 <36313756+skq024@users.noreply.github.com>
2019-02-16 19:19:32 +0530

    Update criterion.py

--- Assignment3/cs763-assign3/Criterion.py
+++ Assignment3/cs763-assign3/Criterion.py
@@ -1,20 +1,19 @@
 import torch
-from torch.autograd import variable
 
 
 class Criterion:
     def forward(inp, target):
+        exponential = torch.exp(inp)
+        softmax = exponential / (torch.sum(exponential, 1, True))
         new = torch.zeros_like(target)
         for i in range(target.size()[0]):
-            new[i][0] = inp[i][target[i]]
+            new[i][0] = softmax[i][target[i]]
         return -torch.mean(torch.log(new))
 
-    def backward(input, target):
-        new = torch.zeros_like(target)
-        for i in range(target.size()[0]):
-            new[i] = -1 / (inp[i][target[i]])
-        grad = new / target.size()[0]
-        new1 = torch.zeros_like(input)
-        for i in range(target.size()[0]):
-            new1[i][target[i]] = grad[i]
-        return new1
+    def backward(inp, target):
+        exponential = torch.exp(inp)
+        softmax = exponential / (torch.sum(exponential, 1, True))
+        bsize = inp.size(0)
+        softmax[range(bsize), target] -= 1
+        grad = softmax / bsize
+        return grad
