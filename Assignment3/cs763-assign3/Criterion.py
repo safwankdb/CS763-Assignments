1e4312880faa3f6fa5cd5727465e6196209fed3d
skq024 <36313756+skq024@users.noreply.github.com>
2019-02-18 20:54:54 +0530

    Complete

--- Assignment3/cs763-assign3/Criterion.py
+++ Assignment3/cs763-assign3/Criterion.py
@@ -6,31 +6,20 @@ class Criterion:
         bsize = inp.size()[0]
         exponential = torch.exp(inp).float()
         softmax = exponential / (torch.sum(exponential, 1, True))
-        # one = torch.ones_like(target)
-        # target = target - one
-        log_likelihood = -torch.log(softmax[range(bsize), target - 1])
-        loss = torch.sum(log_likelihood) / bsize
-        return loss
-        # new = torch.zeros_like(target).float()
-        # for i in range(target.size()[0]):
-        # new[i][0] = softmax[i][target[i]]
-        # return torch.sum(log_likelihood)/
+        one = torch.ones_like(target)
+        target = target - 1
+        new = torch.zeros_like(target).float()
+        for i in range(target.size()[0]):
+            new[i][0] = softmax[i][target[i]]
+        return -torch.mean(torch.log(new))
 
     def backward(inp, target):
-        # one = torch.ones_like(target)
-        # target = target - one
+        one = torch.ones_like(target)
+        target = target - one
         exponential = torch.exp(inp).float()
         softmax = exponential / (torch.sum(exponential, 1, True))
         bsize = inp.size()[0]
-        softmax[range(bsize), target - 1] -= 1
+        for i in range(bsize):
+            softmax[i][target[i]] = softmax[i][target[i]] - 1
         grad = softmax / bsize
         return grad
-
-        # new = torch.zeros_like(target)
-        # for i in range(target.size()[0]):
-        # new[i] = -1 / (inp[i][target[i]])
-        # grad = new / target.size()[0]
-        # new1 = torch.zeros_like(input)
-        # for i in range(target.size()[0]):
-        # new1[i][target[i]] = grad[i]
-        # return new1
