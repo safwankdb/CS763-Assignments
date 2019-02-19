6d360033dadb2b72c433d72c4ab4ea3d62f52a9c
skq024 <36313756+skq024@users.noreply.github.com>
2019-02-19 11:14:23 +0530

    Update

--- Assignment3/cs763-assign3/Criterion.py
+++ Assignment3/cs763-assign3/Criterion.py
@@ -4,19 +4,17 @@ import torch
 class Criterion:
     def forward(inp, target):
         bsize = inp.size()[0]
-        exponential = torch.exp(inp).float()
+        exponential = torch.exp(inp).double()
         softmax = exponential / (torch.sum(exponential, 1, True))
-        one = torch.ones_like(target)
         target = target - 1
-        new = torch.zeros_like(target).float()
+        new = torch.zeros_like(target).double()
         for i in range(target.size()[0]):
             new[i][0] = softmax[i][target[i]]
         return -torch.mean(torch.log(new))
 
     def backward(inp, target):
-        one = torch.ones_like(target)
-        target = target - one
-        exponential = torch.exp(inp).float()
+        target = target - 1
+        exponential = torch.exp(inp).double()
         softmax = exponential / (torch.sum(exponential, 1, True))
         bsize = inp.size()[0]
         for i in range(bsize):
